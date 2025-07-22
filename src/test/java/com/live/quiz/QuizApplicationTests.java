ae71a237c1b041a998fc1cbddaf8511a9a913804
RendyProbe <rendy221205@gmail.com>
2025-07-22 12:19:43 +0700

    springboot

---
+++ src/test/java/com/live/quiz/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
